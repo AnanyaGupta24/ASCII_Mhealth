e0f4ce4ab1020c077c49a3884c80abdf69679123
AnanyaGupta24 <ananya24602@gmail.com>
2022-10-09 13:56:50 +0530

    provider_shopper

---
+++ provider_shopper/lib/common/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+final appTheme = ThemeData(
+  primarySwatch: Colors.orange,
+  backgroundColor: Colors.grey[200],
+  textTheme: const TextTheme(
+    displayLarge: TextStyle(
+      fontFamily: 'NotoSansEthiopic',
+      fontWeight: FontWeight.w700,
+      fontSize: 30,
+      color: Colors.black,
+    ),
+  ),
+);
+
---
+++ provider_shopper/lib/screens/login.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class MyLogin extends StatelessWidget {
+  const MyLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(80.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Welcome',
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Name',
+                ),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Mobile Number',
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/catalog');
+                },
+                child: const Text('ENTER'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
